5a1a04f06e64c760feef0bf1b956d0d9d90a6832
Purnima <saipurnima@outlook.com>
2020-04-18 18:56:15 +0530

    changing covid19 icon sizes

--- covid19/service-worker.js
+++ covid19/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://saipurnimag.github.io/covid19/precache-manifest.5a9a3fd1b3dfaefd5b9175f2ba903404.js"
+  "/precache-manifest.0e466dcf1561f64f075b9e0f81f41ecc.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://saipurnimag.github.io/covid19/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
