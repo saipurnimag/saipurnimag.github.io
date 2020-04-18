20b8e3b226b1d6d1787837a6ff3a4bb1bdc3f312
Purnima <saipurnima@outlook.com>
2020-04-18 18:28:51 +0530

    COVID 19 tracker

---
+++ covid19/precache-manifest.403cddafc2edd83247085df633c55354.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d4747f876c79772599a270d8e535386",
+    "url": "/index.html"
+  },
+  {
+    "revision": "60502de49b6488935df1",
+    "url": "/static/css/2.d788185a.chunk.css"
+  },
+  {
+    "revision": "8591ebe8ce93e2a856cf",
+    "url": "/static/css/main.68091598.chunk.css"
+  },
+  {
+    "revision": "60502de49b6488935df1",
+    "url": "/static/js/2.9cbe592a.chunk.js"
+  },
+  {
+    "revision": "2d8009f6259c9f40bec3e169de950a82",
+    "url": "/static/js/2.9cbe592a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8591ebe8ce93e2a856cf",
+    "url": "/static/js/main.41cfbe74.chunk.js"
+  },
+  {
+    "revision": "d6aff89291f94a07d0f4",
+    "url": "/static/js/runtime-main.a0c160d6.js"
+  },
+  {
+    "revision": "11093ee16c26a61acb6c3c1637e50ec7",
+    "url": "/static/media/logo.11093ee1.svg"
+  },
+  {
+    "revision": "cee6adf3947470925d0bfc7b95cf7e78",
+    "url": "/static/media/notFound.cee6adf3.jpg"
+  }
+]);
